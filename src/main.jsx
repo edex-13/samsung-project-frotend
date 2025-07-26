4dac9507915b4f4e3db75967b6cf8bae3151725a
Ederson Felipe Lopez Bolivar <ederson12f@gmail.com>
2025-07-26 16:29:19 +0000

    solucion de bugs

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/samsung-project-frotend">
       <App />
     </BrowserRouter>
   </React.StrictMode>
